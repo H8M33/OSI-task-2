a00cfebbf417b5a5e4cdd36b2a61c39f4597ffd0
H8M33 <90274360+H8M33@users.noreply.github.com>
2024-01-15 09:20:20 +0300

    Update parent.py

--- parent.py
+++ parent.py
@@ -1,21 +1,31 @@
+#!/usr/bin/python3
 import os
+import random
 import sys
 
-def main():
-    print(f"Parent[{os.getpid()}]: I am started. My PID {os.getpid()}.")
+n = int(sys.argv[1])
 
+print(f"Parent[{os.getpid()}]: I am started. My PID {os.getpid()}.")
+def child_fork():
     child_pid = os.fork()
     if child_pid == 0:
-        os.execve("./child", ["child"], {})
+        os.execl("./child.py", "./child.py", str(random.randint(5, 10)))
     else:
         print(f"Parent[{os.getpid()}]: I ran child process with PID {child_pid}.")
 
     child_pid, status = os.wait()
-    if os.WIFEXITED(status):
-        exit_status = os.WEXITSTATUS(status)
-        print(f"Parent[{os.getpid()}]: Child with PID {child_pid} terminated. Exit Status {exit_status}.")
-    else:
-        print(f"Parent[{os.getpid()}]: Child with PID {child_pid} terminated unexpectedly.")
+    return child
 
-if __name__ == "__main__":
-    main()
+for i in range (n):
+    child = child_fork()
+    
+while n > 0:
+    child_pid, status = os.wait()
+    if status != 0:
+        child = child_fork()
+    else:
+        print(f'Parent[{os.getpid()}]: Child with PID {child_pid} terminated. Exit Status {status}.')
+        n -= 1
+
+os._exit(os.EX_OK)
+
